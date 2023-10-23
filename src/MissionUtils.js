e0e0de9f893834ae8d1476a9b37246af7bfb67f6
scarf <greenscarf005@gmail.com>
2023-10-23 22:55:34 +0900

    chore: add typescript and airbnb checks

---
+++ src/MissionUtils.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {{Console, Random}} MissionUtils
+ */
+export { Console, Random } from '@woowacourse/mission-utils';
+
+/**
+ * @typedef {object} Console
+ * @property {(query: string) => Promise<string>} readLineAsync
+ * @property {(message: string) => void} print
+ */
